a62688c9a9f8f09b2499abd1863ae39999cdea6c
prayag062 <prayag.mishraprayag06@gmail.com>
2026-08-15 15:38:24 +0530

    INITIAL COMMIT

---
+++ PYTHON/input/innput.py
@@ -0,0 +1,6 @@
+
+F_num = int(input("Enter ur first num:"))
+S_num = int(input("Enter ur 2nd num:")
+)
+print("Your first and 2nd num are ", F_num, S_num)
+print("Sum of your first number and second number are",F_num+S_num)
